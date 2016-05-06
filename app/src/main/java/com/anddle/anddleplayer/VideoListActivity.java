5c8b50e884b43d83d6dcbc26997703ad608586c0
Gym <gym_zhang@126.com>
2016-05-06 10:54:42 +0800

    创建工程；

---
+++ app/src/main/java/com/anddle/anddleplayer/VideoListActivity.java
@@ -0,0 +1,13 @@
+package com.anddle.anddleplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VideoListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_list);
+    }
+}
